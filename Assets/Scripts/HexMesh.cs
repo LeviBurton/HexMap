72a6a324a2145708d380a5f4cec8b0ab0d80e888
Levi Burton <levi.burton@outlook.com>
2019-08-25 13:23:11 -0500

    removed commented out triangulation code.

--- Assets/Scripts/HexMesh.cs
+++ Assets/Scripts/HexMesh.cs
@@ -67,33 +67,6 @@ public class HexMesh : MonoBehaviour
         {
             TriangulateConnection(direction, cell, v1, v2);
         }
-        //Vector3 bridge = HexMetrics.GetBridge(direction);
-        //Vector3 v3 = v1 + bridge;
-        //Vector3 v4 = v2 + bridge;
-
-        //AddQuad(v1, v2, v3, v4);
-
-        //// retrieve all three neighbors and perform two three-way blends.
-        //HexCell prevNeighbor = cell.GetNeighbor(direction.Previous()) ?? cell;
-        //HexCell neighbor = cell.GetNeighbor(direction) ?? cell;
-        //HexCell nextNeighbor = cell.GetNeighbor(direction.Next()) ?? cell;
-
-        //Color bridgeColor = (cell.color + neighbor.color) * 0.5f;
-        //AddQuadColor(cell.color, bridgeColor);
-
-        //AddTriangle(v1, center + HexMetrics.GetFirstCorner(direction), v3);
-        //AddTriangleColor(
-        //    cell.color,
-        //    (cell.color + prevNeighbor.color + neighbor.color) / 3f,
-        //    bridgeColor
-        //);
-
-        //AddTriangle(v2, v4, center + HexMetrics.GetSecondCorner(direction));
-        //AddTriangleColor(
-        //    cell.color,
-        //    bridgeColor,
-        //    (cell.color + neighbor.color + nextNeighbor.color) / 3f
-        //);
     }
 
     void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
