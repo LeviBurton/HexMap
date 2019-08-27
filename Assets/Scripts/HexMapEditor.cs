c4ce657eaad09074c423872903a6a9132e284197
Levi Burton <levi.burton@outlook.com>
2019-08-27 18:05:42 -0500

    HexMap 5

--- Assets/Scripts/HexMapEditor.cs
+++ Assets/Scripts/HexMapEditor.cs
@@ -4,59 +4,91 @@ using UnityEngine.EventSystems;
 public class HexMapEditor : MonoBehaviour {
 
 	public Color[] colors;
+
 	public HexGrid hexGrid;
 
-    Color activeColor;
-    int activeElevation;
-    bool renderWireframe;
+	int activeElevation;
 
-    void Awake()
-    {
-        SelectColor(0);
-    }
+	Color activeColor;
 
-    void Update()
-    {
-        var mouseButton = Input.GetMouseButton(0);
-        var isPointerOverGameObject = EventSystem.current.IsPointerOverGameObject();
+	int brushSize;
 
-        if (mouseButton && !isPointerOverGameObject)
-        {
-            HandleInput();
-        }
-    }
+	bool applyColor;
+	bool applyElevation = true;
 
-    void HandleInput()
-    {
-        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
+	public void SelectColor (int index) {
+		applyColor = index >= 0;
+		if (applyColor) {
+			activeColor = colors[index];
+		}
+	}
 
-        if (Physics.Raycast(inputRay, out hit))
-        {
-            EditCell(hexGrid.GetCell(hit.point));
-        }
-    }
+	public void SetApplyElevation (bool toggle) {
+		applyElevation = toggle;
+	}
 
-    void EditCell(HexCell cell)
-    {
-        cell.color = activeColor;
-        cell.Elevation = activeElevation;
+	public void SetElevation (float elevation) {
+		activeElevation = (int)elevation;
+	}
 
-        hexGrid.Refresh();
-    }
+	public void SetBrushSize (float size) {
+		brushSize = (int)size;
+	}
 
-    public void SetElevation(float elevation)
-    {
-        activeElevation = (int)elevation;
-    }
-
-    public void SelectColor(int index)
-    {
-        activeColor = colors[index];
-    }
+	public void ShowUI (bool visible) {
+		hexGrid.ShowUI(visible);
+	}
 
     public void EnableWireframe(bool enabled)
     {
         hexGrid.EnableWireframe(enabled);
     }
+
+    void Awake () {
+		SelectColor(0);
+	}
+
+	void Update () {
+		if (
+			Input.GetMouseButton(0) &&
+			!EventSystem.current.IsPointerOverGameObject()
+		) {
+			HandleInput();
+		}
+	}
+
+	void HandleInput () {
+		Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(inputRay, out hit)) {
+			EditCells(hexGrid.GetCell(hit.point));
+		}
+	}
+
+	void EditCells (HexCell center) {
+		int centerX = center.coordinates.X;
+		int centerZ = center.coordinates.Z;
+
+		for (int r = 0, z = centerZ - brushSize; z <= centerZ; z++, r++) {
+			for (int x = centerX - r; x <= centerX + brushSize; x++) {
+				EditCell(hexGrid.GetCell(new HexCoordinates(x, z)));
+			}
+		}
+		for (int r = 0, z = centerZ + brushSize; z > centerZ; z--, r++) {
+			for (int x = centerX - brushSize; x <= centerX + r; x++) {
+				EditCell(hexGrid.GetCell(new HexCoordinates(x, z)));
+			}
+		}
+	}
+
+	void EditCell (HexCell cell) {
+		if (cell) {
+			if (applyColor) {
+				cell.Color = activeColor;
+			}
+			if (applyElevation) {
+				cell.Elevation = activeElevation;
+			}
+		}
+	}
 }
