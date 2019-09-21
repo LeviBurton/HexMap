12964441dbb155126df8f86830e579b0f323e290
Levi Burton <levi.burton@outlook.com>
2019-09-20 19:40:33 -0500

    HexMap 15 - starting distances

--- Assets/Materials/Shaders/TerrainSurf.hlsl
+++ Assets/Materials/Shaders/TerrainSurf.hlsl
@@ -3,7 +3,7 @@
 #define TERRAINSURFHLSLINCLUDE_INCLUDED
 
 // vertexColor stores splat map
-float4 GetTerrainColor(float4 vertexColor, float3 worldPos, float4 terrain, int index, Texture2DArray terrainTex, SamplerState ss)
+float4 GetTerrainColor_float(float4 vertexColor, float3 worldPos, float4 terrain, int index, Texture2DArray terrainTex, SamplerState ss)
 {
 	float4 c = SAMPLE_TEXTURE2D_ARRAY(terrainTex, ss, worldPos.xz * 0.02, terrain[index]);
 	return c * vertexColor[index];
@@ -11,9 +11,9 @@ float4 GetTerrainColor(float4 vertexColor, float3 worldPos, float4 terrain, int 
 
 void Surf_float(float4 vertexColor, float3 worldPos, float4 terrain, Texture2DArray terrainTex, SamplerState ss, out float4 OutColor)
 {
-	float4 c = GetTerrainColor(vertexColor, worldPos, terrain, 0, terrainTex, ss) +
-		GetTerrainColor(vertexColor, worldPos, terrain, 1, terrainTex, ss) +
-		GetTerrainColor(vertexColor, worldPos, terrain, 2, terrainTex, ss);
+	float4 c = GetTerrainColor_float(vertexColor, worldPos, terrain, 0, terrainTex, ss) +
+		GetTerrainColor_float(vertexColor, worldPos, terrain, 1, terrainTex, ss) +
+		GetTerrainColor_float(vertexColor, worldPos, terrain, 2, terrainTex, ss);
 
 	OutColor = c;
 }
