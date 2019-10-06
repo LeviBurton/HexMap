155c50300eee41354493eb4084dab9b66ffb98d4
Levi Burton <levi.burton@outlook.com>
2019-10-06 17:29:10 -0500

    HexMap 21 -- load/save explored cells.

--- Assets/Scripts/HexCell.cs
+++ Assets/Scripts/HexCell.cs
@@ -610,9 +610,10 @@ public class HexCell : MonoBehaviour
             }
         }
         writer.Write((byte)roadFlags);
+        writer.Write(IsExplored);
     }
 
-    public void Load(BinaryReader reader)
+    public void Load(BinaryReader reader, int header) 
     {
         terrainTypeIndex = reader.ReadByte();
         ShaderData.RefreshTerrain(this);
@@ -652,6 +653,8 @@ public class HexCell : MonoBehaviour
         {
             roads[i] = (roadFlags & (1 << i)) != 0;
         }
+        IsExplored = header >= 3 ? reader.ReadBoolean() : false;
+        ShaderData.RefreshVisibility(this);
     }
 
     public void SetLabel(string text)
